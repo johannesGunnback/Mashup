9b74ddcee9841c4bd729bd939de70f25ef9e1c8b
johangun <johannes.gunnback@tele2.com>
2020-04-10 10:43:08 +0200

    base project from spring initializr

---
+++ src/test/java/se/jg/mashup/MashupApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jg.mashup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MashupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
